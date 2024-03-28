d74a9feee3661a10cab0b37f748a93e499f5c63a
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-03-28 22:36:32 +0100

    Add Session.whenActive

--- modules/app/src/main/scala/com/rockthejvm/reviewboard/core/Session.scala
+++ modules/app/src/main/scala/com/rockthejvm/reviewboard/core/Session.scala
@@ -3,12 +3,25 @@ package com.rockthejvm.reviewboard.core
 import com.rockthejvm.reviewboard.domain.data.UserToken
 import com.raquo.laminar.api.L.{*, given}
 import scala.scalajs.js.Date
+import com.raquo.airstream.core.Signal
+import com.raquo.laminar.receivers.ChildReceiver
 
 object Session {
   val userState: Var[Option[UserToken]] = Var(Option.empty[UserToken])
 
   private val userTokenKey = "userToken"
 
+  /** This method is used to produce an Option when the user is active.
+    *
+    * Convenient to render an element only when the user is active.
+    *
+    * See ChildReceiver.maybe for more information.
+    *
+    * @param callback
+    * @return
+    */
+  def whenActive[A](callback: => A): Signal[Option[A]] =
+    userState.signal.map(_.map(_ => callback))
   // Should be more clever about expiration.
   def isActive = userState.now().isDefined
 
