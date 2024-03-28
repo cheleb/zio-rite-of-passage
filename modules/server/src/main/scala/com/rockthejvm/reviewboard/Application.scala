cf5e8aaae8ca03090f9e8987bc080d9041d6e8ee
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-03-28 22:31:28 +0100

    Log HttpRequest

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
@@ -14,17 +14,36 @@ import com.rockthejvm.reviewboard.config.Configs
 import com.rockthejvm.reviewboard.config.JWTConfig
 import com.rockthejvm.reviewboard.config.EmailServiceConfig
 import sttp.tapir.server.interceptor.cors.CORSInterceptor
+import sttp.tapir.server.interceptor.log.DefaultServerLog
+import com.rockthejvm.reviewboard.Application.serverLOg
+import sttp.tapir.server.interceptor.log.ServerLog
+import zio.logging.backend.SLF4J
 
 object Application extends ZIOAppDefault:
 
+  val serverLOg: ServerLog[Task] = DefaultServerLog(
+    doLogWhenReceived = { s => ZIO.logInfo(s">>> $s") },
+    doLogWhenHandled = { case (s, e) => ZIO.logInfo(s">>> $s: $e") },
+    doLogAllDecodeFailures = { case (s, e) => ZIO.logInfo(s">>> $s: $e") },
+    doLogExceptions = { case (s, e) => ZIO.logInfo(s">>> $s: $e") },
+    noLog = ZIO.logInfo("........................."),
+    logWhenReceived = true,
+    logWhenHandled = true,
+    logAllDecodeFailures = true
+  )
+
+  override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
+
   val serrverProgram =
     for
-      _         <- ZIO.succeed(println("Hello world"))
+      _         <- ZIO.logWarning("Running the server")
       endpoints <- HttpApi.endpointsZIO
       _ <- Server.serve(
-        ZioHttpInterpreter(ZioHttpServerOptions.default.appendInterceptor(
-          CORSInterceptor.default
-        )).toHttp(endpoints)
+        ZioHttpInterpreter(ZioHttpServerOptions
+          .customiseInterceptors.serverLog(serverLOg).options
+          .appendInterceptor(
+            CORSInterceptor.default
+          )).toHttp(endpoints)
       )
     yield ()
 
