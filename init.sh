ef5e6e448847eef368505362bb1cbbbef22e5a4c
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-01-05 23:37:01 +0100

    fix: update setup command to use init.sh and enhance script logic

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
-sbt fullstackInit
-code .
+#!/usr/bin/env bash
+
+if [ ! -e "./scripts-managed/setup.sc" ]; then
+  echo "✨ Setting up fullstack environment..."
+  sbt fullstackInit
+fi
+
+./scripts-managed/setup.sc
+
